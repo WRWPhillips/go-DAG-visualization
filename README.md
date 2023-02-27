3325bd9275a3a7c9e2e2d7b0d1ffb92389c86829
WRWPhillips <42WPhillips42@gmail.com>
2023-02-27 11:57:52 -0600

    More demonstrative example

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a proof of concept and small project to help demystify DAGs for myself.
--- main.go
+++ main.go
@@ -12,19 +12,26 @@ func main() {
 	g := graph.New(graph.IntHash, graph.Directed(), graph.Acyclic())
 
 	// adding 5 vertices
-	_ = g.AddVertex(1)
-	_ = g.AddVertex(2)
-	_ = g.AddVertex(3)
-	_ = g.AddVertex(4)
-	_ = g.AddVertex(5)
+	_ = g.AddVertex(1, graph.VertexAttribute("label", "commit 1"))
+	_ = g.AddVertex(2, graph.VertexAttribute("label", "commit 2"))
+	_ = g.AddVertex(3, graph.VertexAttribute("label", "commit 3"))
+	_ = g.AddVertex(4, graph.VertexAttribute("label", "commit 4"))
+	_ = g.AddVertex(5, graph.VertexAttribute("label", "commit 1"))
+	_ = g.AddVertex(6, graph.VertexAttribute("label", "commit 5"))
+	_ = g.AddVertex(7, graph.VertexAttribute("label", "commit 2"))
+	_ = g.AddVertex(8, graph.VertexAttribute("label", "merge into main, commit 6"))
+	_ = g.AddVertex(9, graph.VertexAttribute("label", "commit 7"))
 
 	// adding edges between vertices
-	_ = g.AddEdge(1, 2)
-	_ = g.AddEdge(2, 3)
-	_ = g.AddEdge(3, 4)
-	_ = g.AddEdge(3, 5)
-	_ = g.AddEdge(5, 6)
-	_ = g.AddEdge(4, 6)
+	_ = g.AddEdge(1, 2, graph.EdgeAttribute("label", "main"))
+	_ = g.AddEdge(2, 3, graph.EdgeAttribute("label", "main"))
+	_ = g.AddEdge(3, 4, graph.EdgeAttribute("label", "main"))
+	_ = g.AddEdge(3, 5, graph.EdgeAttribute("label", "feature"))
+	_ = g.AddEdge(5, 7, graph.EdgeAttribute("label", "feature"))
+	_ = g.AddEdge(4, 6, graph.EdgeAttribute("label", "main"))
+	_ = g.AddEdge(6, 8, graph.EdgeAttribute("label", "main"))
+	_ = g.AddEdge(7, 8, graph.EdgeAttribute("label", "merge"))
+	_ = g.AddEdge(8, 9, graph.EdgeAttribute("label", "main"))
 
 	// create empty file
 	file, _ := os.Create("./DAG-graph.gv")
